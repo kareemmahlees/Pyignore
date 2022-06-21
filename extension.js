588d35aea2e71914d1155fa73fccf1d81a05b18e
kareem <kareemmahlees@gmail.com>
2022-06-21 23:44:33 +0200

    ✅ created base checker for workspaces check if workspaces exists and if more the one exists prompt the user with input box to type the name will be updated in future so that user could choose between options if no workspaces opend error message will pop

--- extension.js
+++ extension.js
@@ -1,5 +1,4 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
 // this method is called when your extension is activated
@@ -10,23 +9,14 @@ const vscode = require('vscode');
  */
 async function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	await console.log('Congratulations, your extension "python--gitignore" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('python--gitignore.helloWorld', function () {
-	// The code you place here will be executed every time your command is executed
-
-	// Display a message box to the user
-	// vscode.window.showInformationMessage('Hello World from Python .gitignore!');
-	// });
 	let disposable = await vscode.commands.registerCommand('python--gitignore.generate', async function () {
-		// console.log(vscode.workspace.fs.readDirectory())
 		try {
+			// if more than one workspace is present
 			if (vscode.workspace.workspaceFolders.length > 1) {
+				// prompt the user to input the name of the workspace
 				const activeDir = await vscode.window.showInputBox();
 				console.log(activeDir);
 			} else {
@@ -36,19 +26,9 @@ async function activate(context) {
 
 		}
 		catch (error) {
+			// if no workspaces found 
 			vscode.window.showErrorMessage("No Folder Is Opened")
 		}
-		// })
-		// vscode.
-		// const wsedit = new vscode.WorkspaceEdit();
-		// const wsPath = vscode.window.showWorkspaceFolderPick()
-		// console.log(wsPath)
-
-		// vscode.window.showInformationMessage("run successfuly")
-		// const filePath = vscode.Uri.file(wsPath + '/.gitignore');
-		// vscode.window.showInformationMessage(filePath.toString());
-		// vscode.workspace.applyEdit(wsedit);
-		// vscode.window.showInformationMessage('Created a new file: .gitignore');
 
 	})
 
