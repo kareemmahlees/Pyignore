5b6c2fbc2580110dfc1b39f8910559b97bc65b36
kareem <kareemmahlees@gmail.com>
2022-06-23 20:05:22 +0200

    �  take workspace name from user and fetch it's path get the names of the opened workspaces list them for the user and get him to choose the desired one get the path of the choosen workspace

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 const vscode = require('vscode');
+// const path_util = require('path')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,15 +11,30 @@ const vscode = require('vscode');
 async function activate(context) {
 
 	// This line of code will only be executed once when your extension is activated
-	await console.log('Congratulations, your extension "python--gitignore" is now active!');
+	console.log('Congratulations, your extension "python--gitignore" is now active!');
 
-	let disposable = await vscode.commands.registerCommand('python--gitignore.generate', async function () {
+	let disposable = vscode.commands.registerCommand('python--gitignore.generate', async function () {
 		try {
+			const workspaces = vscode.workspace.workspaceFolders;
 			// if more than one workspace is present
-			if (vscode.workspace.workspaceFolders.length > 1) {
-				// prompt the user to input the name of the workspace
-				const activeDir = await vscode.window.showInputBox();
-				console.log(activeDir);
+			if (workspaces.length > 1) {
+				// prompt the user to choose the name of the workspace
+				let arr1 = [];
+				let arr2 = [];
+				for (let x of workspaces) {
+					//array containing only the names of workspaces to display them as quickpicks
+					arr1.push(x.name)
+					//array containing the names and paths of workspaces for creating the file
+					arr2.push([x.name, x.uri.path])
+				}
+				// eslint-disable-next-line no-unused-vars 
+				const pick = await vscode.window.showQuickPick(arr1)
+				for (let [x, y] of arr2) {
+					if (x == pick) {
+						// eslint-disable-next-line no-unused-vars 
+						const pathToWorkspace = y
+					}
+				}
 			} else {
 				await vscode.window.showInformationMessage("Creating .gitignore")
 
@@ -27,7 +43,7 @@ async function activate(context) {
 		}
 		catch (error) {
 			// if no workspaces found 
-			vscode.window.showErrorMessage("No Folder Is Opened")
+			vscode.window.showErrorMessage("No Folder Is Opened , Please Open A Folder")
 		}
 
 	})
@@ -42,3 +58,4 @@ module.exports = {
 	activate,
 	deactivate
 }
+
